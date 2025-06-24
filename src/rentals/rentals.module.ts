ae2cadb5a3472b626b77ce8bcd1b70ec508fe5c5
Harsh Nirmal <harsh.nirmal@esparkbizmail.com>
2025-06-24 14:05:59 +0530

    small change

--- src/rentals/rentals.module.ts
+++ src/rentals/rentals.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
-import { RentalsService } from './rentals.service';
 import { RentalsController } from './rentals.controller';
 
 @Module({
   controllers: [RentalsController],
-  providers: [RentalsService],
+  providers: [],
 })
 export class RentalsModule {}
